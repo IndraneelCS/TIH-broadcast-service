0f6a6c0cf70411f8113b70b703694857a6ecb415
Indraneel C S <indraneelcs@gmail.com>
2021-11-23 02:31:46 +0530

    Added Video feed

--- src/main/webapp/app/feed/feed.component.ts
+++ src/main/webapp/app/feed/feed.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { PushnotificationService } from '../entities/pushnotification/service/pushnotification.service';
+import { IPushnotification, Pushnotification } from '../entities/pushnotification/pushnotification.model';
+import { Observable } from 'rxjs';
+import { finalize } from 'rxjs/operators';
+import { HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'jhi-feed',
@@ -8,6 +12,12 @@ import { PushnotificationService } from '../entities/pushnotification/service/pu
 })
 export class FeedComponent implements OnInit {
   feed = true;
+  isSaving = false;
+
+  pushnotification1 = this.createFromForm('Amateur', 'Goal', 'LAST MINUTE GOAL!!!!', 'Renegades have won it at the very last minute');
+  pushnotification2 = this.createFromForm('Casual', 'All', 'LAST MINUTE GOAL!!!!', 'Renegades have won it at the very last minute');
+  pushnotification3 = this.createFromForm('Professional', 'High', 'LAST MINUTE GOAL!!!!', 'Renegades have won it at the very last minute');
+  pushnotification4 = this.createFromForm('Amateur', 'Goal', 'LAST MINUTE GOAL!!!!', 'Renegades have won it at the very last minute');
 
   constructor(protected pushnotificationService: PushnotificationService) {
     this.feed = true;
@@ -19,5 +29,47 @@ export class FeedComponent implements OnInit {
 
   save(): void {
     this.pushnotificationService.create;
+  }
+
+  previousState(): void {
+    window.history.back();
+  }
+
+  click1(): void {
+    this.isSaving = true;
+    this.subscribeToSaveResponse(this.pushnotificationService.create(this.pushnotification1));
+    this.subscribeToSaveResponse(this.pushnotificationService.create(this.pushnotification2));
+    this.subscribeToSaveResponse(this.pushnotificationService.create(this.pushnotification3));
+    this.subscribeToSaveResponse(this.pushnotificationService.create(this.pushnotification4));
+  }
+
+  protected subscribeToSaveResponse(result: Observable<HttpResponse<IPushnotification>>): void {
+    result.pipe(finalize(() => this.onSaveFinalize())).subscribe(
+      () => this.onSaveSuccess(),
+      () => this.onSaveError()
+    );
+  }
+
+  protected onSaveSuccess(): void {
+    this.previousState();
+  }
+
+  protected onSaveError(): void {
+    // Api for inheritance.
+  }
+
+  protected onSaveFinalize(): void {
+    this.isSaving = false;
+  }
+
+  protected createFromForm(fanLevel: string, priority: string, header: string, message: string): IPushnotification {
+    return {
+      ...new Pushnotification(),
+      fanLevel: fanLevel,
+      priority: priority,
+      messageHeader: header,
+      message: message,
+      timestamp: undefined,
+    };
   }
 }
